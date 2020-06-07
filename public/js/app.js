bd6c3fba13d1dc74aa84f3796084b8c363f531de
kadocho12 <baseballtaichi511218@gmail.com>
2020-06-07 23:18:29 +0900

    フロントエンド準備調整

--- public/js/app.js
+++ public/js/app.js
@@ -570,7 +570,9 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c("main", [_c("div", { staticClass: "container" }, [_c("RouterView")], 1)])
+    _c("main", [
+      _c("div", { staticClass: "container" }, [_c("router-view")], 1)
+    ])
   ])
 }
 var staticRenderFns = []
